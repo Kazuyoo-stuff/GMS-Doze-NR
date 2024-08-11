19ce8b8f198beb72bec3ad6ab1b45b41523432ab
Kazuyoo_12 <dikinurrohmat42@gmail.com>
2024-08-11 14:42:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,17 +19,6 @@ ANDROIDVERSION=$(getprop ro.build.version.release)
 DATE="12 - 5 - 2024"
 DEVICES=$(getprop ro.product.board)
 MANUFACTURER=$(getprop ro.product.manufacturer)
-version=$(cat /sdcard/Doze/run.sh | grep 1.3)
-
-# compare versions
-if ([ "$version" == "VERSION=1.3" ]); then
-    Launch=1.3
-else
-    echo "[ ! ] old version not the latest."
-    echo "      the latest version is $VERSION"
-    echo "      bye bye"
-    exit 1
-fi
 
 # // The message that appears in the terminal
 sleep 0.5
